2a6ccf39ab8d97000ad0b907106aafa7001d3278
nicktranuno <nicholastran@unomaha.edu>
2026-05-08 13:08:00 -0500

    fade in animation

---
+++ script.js
@@ -0,0 +1,19 @@
+const cards = document.querySelectorAll(".project-card");
+
+const observer = new IntersectionObserver((entries) => {
+
+    entries.forEach((entry) => {
+
+        if (entry.isIntersecting) {
+
+            entry.target.classList.add("show");
+        }
+    });
+
+}, {
+    threshold: 0.15
+});
+
+cards.forEach((card) => {
+    observer.observe(card);
+});
